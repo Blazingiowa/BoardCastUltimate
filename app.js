e280062d190e73986b4c9b860ebeb554d64187ef
shikishima <yasutaka913@gmail.com>
2021-11-04 00:45:03 -0700

    ボタン不具合発生中

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 /**
- * /app.js
+ * function
+ */
+
+function ExpressRouting(url){
+    var exp=express.static(path.join(__dirname,url));
+    return exp;
+}
+
+/**
+ * app.js
  */
 
 //express モジュールインスタンス生成
+const crypto=require('crypto');
 const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
@@ -17,22 +27,54 @@ const path = require('path');
 //静的ファイルのルーティング
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.use('/chat', express.static(path.join(__dirname, 'public/chatroom.html')));
+app.use('/chat', ExpressRouting('public/chatroom.html'));
+
+app.use('/deb',ExpressRouting('public/titlewindow.html'));
 //その他リクエストに対するエラー
 app.use((req, res) => {
     res.sendStatus(404);
 });
 
-//Socket処理
+/**
+ * Socket処理
+ */
 io.on('connection', function (socket) {
-    console.log('接続されています！');
+    //ユーザが接続
+    console.log('Socketが使用されました');
 
-    socket.on('message', function (msg) {
+    /**
+     * ログイン
+     */
+    (()=>{
+        //トークン作成
+        const token=makeToken(socket.id);
+
+        //ご本人にトークンを送る
+        io.to(socket.id).emit("token",{token:token});
+    })();
+
+    /*socket.on('message', function (msg) {
         console.log(msg);
-    });
+    });*/
+
+    socket.on("post",(msg)=>{
+        io.emit("member-post",msg)
+    })
 });
+
+/**
+ * トークン生成
+ * 
+ * @param {string} id - socket.id
+ * @return {string}
+ */
+function makeToken(id){
+    const str="ouijaboarddemon"+id;
+    return(crypto.createHash("sha1").update(str).digest('hex'));
+}
+
 
 //3000番で待受
 http.listen(port, () => {
-    console.log('3000番ポートで待受なう');
+    console.log('3000番ポートで待受中です');
 });
--- public/js/boardcastultimate.js
+++ public/js/boardcastultimate.js
@@ -1,14 +1,80 @@
 /**
  * socket.io宣言
  */
+const WHOAMI={
+    token:null,//トークン
+    name:null //名前
+}
 $(function () {
-    var socket = io();
-
-    $('#message_form').submit(function () {
-        socket.emit('message', $('#input_msg').val());
-        $('#input_msg').val('');
+    //送信ボタンが押されたら
+    $(".btn-contain").click(function(){
+        $("#message_form").submit();
         return false;
     })
+
+    const socket=io();
+
+    //正常に接続したら・・・
+    socket.on("connect",()=>{
+        console.log("接続されました");
+    });
+
+    //トークン発行されましたら・・・
+    socket.on("token",(data)=>{
+        WHOAMI.token=data.token;
+    });
+
+    /**
+     * チャット処理
+     */
+
+    $("#message_form").submit(function(){
+
+        //入力内容取得
+        const msg=$("#artist").val();
+        console.log(msg);
+        if(msg===""){
+            return (false);
+        }
+
+        //Socket.ioサーバへ送信
+        socket.emit("post",{
+            text:msg,
+            token:WHOAMI.token
+        });
+
+        //発言フォームリセット
+        $("#artist").val("");
+
+
+    });
+
+    /**
+     * 誰かが発言
+     */
+    socket.on("member-post",(msg)=>{
+        const is_me=(msg.token===WHOAMI.token);
+        addMessage(msg,is_me);
+    });
+
+    /**
+     * 発言を表示する
+     * 
+     * @param {object} msg
+     * @param {boolean} [is_me=false]
+     * @return {void}
+     */
+
+    function addMessage(msg,is_me=false){
+        const list=$(".chat-thread");
+
+        if(is_me){
+            console.log("自分の発言です");
+        }
+        else{
+            console.log("相手の発言です");
+        }
+    }
 });
 
 //日本地図生成
